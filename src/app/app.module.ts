042a074d3abccb92960f27216b0fade33c07e812
Rasha <engrasha646@gmail.com>
2019-12-29 18:48:01 +0200

    very good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AccordionModule, CarouselModule } from 'ngx-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    CarouselModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
